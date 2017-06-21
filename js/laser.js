a01afd63cfdfff1cc45aa166fd92a10c4b223128
Zakor Gyula <zakorgy@inf.u-szeged.hu>
2017-06-21 16:05:27 +0200

    fixup! Laser wip

--- js/laser.js
+++ js/laser.js
@@ -1,26 +1,44 @@
 game.Laser = me.Entity.extend({
     init : function (x, y, angle) {
-        this._super(me.Entity, "init", [x, y, {width: 6, height: (me.game.viewport.width * 1.5)}]);
+        this._super(me.Entity, "init", [x, y, {width: 6, height: (300)}]);
         this.z = 5;
         this.body.collisionType = game.collisionTypes.LASER;
+
+        // TODO: Calculate the correct length of the laser to the edge of the viewport.
+        this.body.addShape(new me.Rect(0, 0, 6, 300).rotate(angle - Math.PI / 2), false);
+        this.body.shapes.shift();
+
+        var bounds = this.getBounds();
+        this.boundsCenterX = bounds.top + bounds.height / 2;
+        this.boundsCenterY = bounds.left + bounds.width / 2;
+        var xDistToBoundsCenter =  x - this.boundsCenterX;
+        var yDistToBoundsCenter =  y - this.boundsCenterY;
+        //console.log("center: " + this.boundsCenterX + " " + this.boundsCenterY);
+        console.log("distances: " + xDistToBoundsCenter + "  "+ yDistToBoundsCenter);
         this.renderable = new (me.Renderable.extend({
             init : function () {
-                this._super(me.Renderable, "init", [0, 0, -6, -300]);
+                this._super(me.Renderable, "init", [-6, 0, -6, -300]);
             },
             destroy : function () {},
             draw : function (renderer) {
                 var color = renderer.getColor();
                 renderer.setColor('#5E007E');
-                renderer.fillRect(0, 0, -this.width, -300);
+                //renderer.translate(-3, 0);
+                //renderer.rotate(angle + Math.PI / 2);
+                //renderer.translate(3, 0);
+                //renderer.fillRect(-6, 0, -this.width, -300);
+                //renderer.translate(-3, 0);
+                //renderer.rotate(-angle - Math.PI / 2);                
+                //renderer.translate(3, 0);
+                renderer.translate(-this.xDistToBoundsCenter, this.yDistToBoundsCenter);
+                renderer.fillRect(-6, 0, -this.width, -300);
+                renderer.translate(this.xDistToBoundsCenter, -this.yDistToBoundsCenter);
                 renderer.setColor(color);
             }
         }));
 
-        // TODO: Calculate the correct length of the laser to the edge of the viewport.
-        this.body.addShape(new me.Rect(0, 0, 6, me.game.viewport.width * 1.5).rotate(angle - Math.PI / 2), false);
-        this.body.shapes.shift();
-        this.renderable.currentTransform.identity().translate(x, y);
-        this.renderable.currentTransform.rotate(angle);
+        //this.renderable.currentTransform.identity().translate(x, y);
+        //this.renderable.currentTransform.rotate(angle);
 
         this.alwaysUpdate = false;
         this.damage = 10;
