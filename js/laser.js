9d98bfa11d71b685e7c9fe7934c139f88c3ff4ff
zakorgy <zakorgy@inf.u-szeged.hu>
2017-06-22 23:31:50 +0200

    fixup! fixup! Laser wip

--- js/laser.js
+++ js/laser.js
@@ -14,8 +14,42 @@ game.Laser = me.Entity.extend({
         this.body.addShape(new me.Rect(32  - 3 * Math.sin(angle), 32 + 3 * Math.cos(angle), 6, 300).rotate(angle - Math.PI / 2), false);
         this.body.shapes.shift();
 
-        this.renderable._bounds.setShape(x, y, 1, 1);
-        this.renderable.currentTransform.translate(-32 - 3 * Math.sin(angle), 24 + 3 * Math.cos(angle));
+        //this.renderable._bounds.setShape(x, y, 1, 1);
+        //this.renderable.currentTransform.translate(-32 - 3 * Math.sin(angle), 24 + 3 * Math.cos(angle));
+        if (angle > 0 && angle <= 1.66)
+        {
+            this.anchorPoint.set(0.0, 0.0);
+            this.renderable.anchorPoint.set(0,0);
+            this.renderable.currentTransform.translate(24, 24);
+            console.log("1");
+        
+        }
+        if (angle > 1.66 && angle <= Math.PI)
+        {
+            this.anchorPoint.set(0.0, 0.0);
+            this.renderable.anchorPoint.set(0,0);
+            this.renderable.currentTransform.translate(-this.renderable.getBounds().width * Math.cos(angle), 24);
+            console.log("2");
+        }
+        if (angle > -Math.PI && angle <= -1.66)
+        {
+            /*this.anchorPoint.set(0.0, 0.0);
+            this.renderable.anchorPoint.set(0,0);
+            this.renderable.currentTransform.translate(-Math.cos(angle) * 324, -Math.sin(angle) * 324);
+            console.log("3");*/
+            this.anchorPoint.set(0.5, 1.0);
+            this.renderable.anchorPoint.set(1,0);
+            this.renderable.currentTransform.translate(Math.cos(angle) * 200, Math.sin(angle) * 300);
+            console.log("3");
+        }
+        if (angle > -1.66 && angle <= 0)
+        {
+            this.anchorPoint.set(0.5, 1.0);
+            this.renderable.anchorPoint.set(0.0,0.0);
+            this.renderable.currentTransform.translate(-this.renderable.getBounds().width / 2 + 24, 0);
+            console.log("4");
+        }
+        //console.log(angle);
         this.renderable.currentTransform.rotate(angle);     
         this.alwaysUpdate = false;
         this.damage = 10;
